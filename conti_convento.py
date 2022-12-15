c873aae0436be57df468ff7343069a41500472ce
stefano <giacomo.internet@gmail.com>
2022-12-15 11:27:50 +0100

    Frame calc + calc

--- conti_convento.py
+++ conti_convento.py
@@ -53,7 +53,7 @@ title.pack(side=TOP, fill=X)
 Frame1 = Frame(root, bd='4', bg='blue', relief=RIDGE)
 Frame1.place(x=20, y=85, width=550, height=850)
 #FRame calcolatrice
-Frame_calc=Frame(Frame1, bd='4', bg='blue', relief=RIDGE)
+Frame_calc = Frame(Frame1, bd='4', bg='blue', relief=RIDGE)
 Frame_calc.place(x=15, y=468, width=500, height=290)
 # Frame 1 - bottom side Frame
 Frame1in = Frame(Frame1, bd='4', bg='blue', relief=RIDGE)
@@ -111,6 +111,171 @@ Frame1_VoceSpesa.grid(row=10, padx=25, pady=20, sticky='w')
 Frame1_Euro = Label(Frame1, text='Euro', font=('verdana', 15, 'bold'), bg='blue', fg='white')
 Frame1_Euro.grid(row=12, padx=25, pady=20, sticky='w')
 #######################
+
+# Python program to create a simple GUI
+# calculator using Tkinter
+
+# import everything from tkinter module
+#from tkinter import *
+
+# globally declare the expression variable
+expression = ""
+
+
+# Function to update expression
+# in the text entry box
+def press(num):
+	# point out the global expression variable
+	global expression
+
+	# concatenation of string
+	expression = expression + str(num)
+
+	# update the expression by using set method
+	equation.set(expression)
+
+
+# Function to evaluate the final expression
+def equalpress():
+	# Try and except statement is used
+	# for handling the errors like zero
+	# division error etc.
+
+	# Put that code inside the try block
+	# which may generate the error
+	try:
+
+		global expression
+
+		# eval function evaluate the expression
+		# and str function convert the result
+		# into string
+		total = str(eval(expression))
+
+		equation.set(total)
+
+		# initialize the expression variable
+		# by empty string
+		expression = ""
+
+	# if error is generate then handle
+	# by the except block
+	except:
+
+		equation.set(" error ")
+		expression = ""
+
+
+# Function to clear the contents
+# of text entry box
+def clear():
+	global expression
+	expression = ""
+	equation.set("")
+
+
+# Driver code
+# if __name__ == "__main__":
+	# create a GUI window
+	# gui = Tk()
+    #
+	# # set the background colour of GUI window
+	# gui.configure(background="light green")
+    #
+	# # set the title of GUI window
+	# gui.title("Simple Calculator")
+    #
+	# # set the configuration of GUI window
+	# gui.geometry("270x150")
+
+	# StringVar() is the variable class
+	# we create an instance of this class
+equation = StringVar()
+
+	# create the text entry box for
+	# showing the expression .
+expression_field = Entry(Frame_calc, textvariable=equation)
+
+	# grid method is used for placing
+	# the widgets at respective positions
+	# in table like structure .
+expression_field.grid(columnspan=4, ipadx=70)
+
+	# create a Buttons and place at a particular
+	# location inside the root window .
+	# when user press the button, the command or
+	# function affiliated to that button is executed .
+button1 = Button(Frame_calc, text=' 1 ', fg='black', bg='red',
+				command=lambda: press(1), height=1, width=7)
+button1.grid(row=2, column=0)
+
+button2 = Button(Frame_calc, text=' 2 ', fg='black', bg='red',
+				command=lambda: press(2), height=1, width=7)
+button2.grid(row=2, column=1)
+
+button3 = Button(Frame_calc, text=' 3 ', fg='black', bg='red',
+				command=lambda: press(3), height=1, width=7)
+button3.grid(row=2, column=2)
+
+button4 = Button(Frame_calc, text=' 4 ', fg='black', bg='red',
+				command=lambda: press(4), height=1, width=7)
+button4.grid(row=3, column=0)
+
+button5 = Button(Frame_calc, text=' 5 ', fg='black', bg='red',
+				command=lambda: press(5), height=1, width=7)
+button5.grid(row=3, column=1)
+
+button6 = Button(Frame_calc, text=' 6 ', fg='black', bg='red',
+				command=lambda: press(6), height=1, width=7)
+button6.grid(row=3, column=2)
+
+button7 = Button(Frame_calc, text=' 7 ', fg='black', bg='red',
+				command=lambda: press(7), height=1, width=7)
+button7.grid(row=4, column=0)
+
+button8 = Button(Frame_calc, text=' 8 ', fg='black', bg='red',
+				command=lambda: press(8), height=1, width=7)
+button8.grid(row=4, column=1)
+
+button9 = Button(Frame_calc, text=' 9 ', fg='black', bg='red',
+				command=lambda: press(9), height=1, width=7)
+button9.grid(row=4, column=2)
+
+button0 = Button(Frame_calc, text=' 0 ', fg='black', bg='red',
+				command=lambda: press(0), height=1, width=7)
+button0.grid(row=5, column=0)
+
+plus = Button(Frame_calc, text=' + ', fg='black', bg='red',
+				command=lambda: press("+"), height=1, width=7)
+plus.grid(row=2, column=3)
+
+minus = Button(Frame_calc, text=' - ', fg='black', bg='red',
+				command=lambda: press("-"), height=1, width=7)
+minus.grid(row=3, column=3)
+
+multiply = Button(Frame_calc, text=' * ', fg='black', bg='red',
+				command=lambda: press("*"), height=1, width=7)
+multiply.grid(row=4, column=3)
+
+divide = Button(Frame_calc, text=' / ', fg='black', bg='red',
+				command=lambda: press("/"), height=1, width=7)
+divide.grid(row=5, column=3)
+
+equal = Button(Frame_calc, text=' = ', fg='black', bg='red',
+			command=equalpress, height=1, width=7)
+equal.grid(row=5, column=2)
+
+clear = Button(Frame_calc, text='Clear', fg='black', bg='red',
+			command=clear, height=1, width=7)
+clear.grid(row=5, column='1')
+
+Decimal= Button(Frame_calc, text='.', fg='black', bg='red',
+				command=lambda: press('.'), height=1, width=7)
+Decimal.grid(row=6, column=0)
+	# start the GUI
+	#gui.mainloop()
+
+#########################
 
 def pick_Categoria(e):
     if my_combo.get() == "Entrate":
