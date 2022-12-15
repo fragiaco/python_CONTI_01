b813e0ab8c93f7619876265c37788effff73405c
stefano <giacomo.internet@gmail.com>
2022-12-15 11:53:07 +0100

    Frame calc + calc

--- conti_convento.py
+++ conti_convento.py
@@ -53,8 +53,8 @@ title.pack(side=TOP, fill=X)
 Frame1 = Frame(root, bd='4', bg='blue', relief=RIDGE)
 Frame1.place(x=20, y=85, width=550, height=850)
 #FRame calcolatrice
-Frame_calc = Frame(Frame1, bd='4', bg='blue', relief=RIDGE)
-Frame_calc.place(x=15, y=468, width=500, height=290)
+Frame_calc = Frame(Frame1, bd='4', bg='light blue', relief=RIDGE)
+Frame_calc.place(x=220, y=468, width=275, height=185)
 # Frame 1 - bottom side Frame
 Frame1in = Frame(Frame1, bd='4', bg='blue', relief=RIDGE)
 Frame1in.place(x=15, y=768, width=500, height=60)
@@ -205,71 +205,71 @@ expression_field.grid(columnspan=4, ipadx=70)
 	# location inside the root window .
 	# when user press the button, the command or
 	# function affiliated to that button is executed .
-button1 = Button(Frame_calc, text=' 1 ', fg='black', bg='red',
+button1 = Button(Frame_calc, text=' 1 ', fg='black', bg='light grey',
 				command=lambda: press(1), height=1, width=7)
-button1.grid(row=2, column=0)
+button1.grid(row=2, column=0, pady=2)
 
-button2 = Button(Frame_calc, text=' 2 ', fg='black', bg='red',
+button2 = Button(Frame_calc, text=' 2 ', fg='black', bg='light grey',
 				command=lambda: press(2), height=1, width=7)
 button2.grid(row=2, column=1)
 
-button3 = Button(Frame_calc, text=' 3 ', fg='black', bg='red',
+button3 = Button(Frame_calc, text=' 3 ', fg='black', bg='light grey',
 				command=lambda: press(3), height=1, width=7)
 button3.grid(row=2, column=2)
 
-button4 = Button(Frame_calc, text=' 4 ', fg='black', bg='red',
+button4 = Button(Frame_calc, text=' 4 ', fg='black', bg='light grey',
 				command=lambda: press(4), height=1, width=7)
-button4.grid(row=3, column=0)
+button4.grid(row=3, column=0, pady=2)
 
-button5 = Button(Frame_calc, text=' 5 ', fg='black', bg='red',
+button5 = Button(Frame_calc, text=' 5 ', fg='black', bg='light grey',
 				command=lambda: press(5), height=1, width=7)
 button5.grid(row=3, column=1)
 
-button6 = Button(Frame_calc, text=' 6 ', fg='black', bg='red',
+button6 = Button(Frame_calc, text=' 6 ', fg='black', bg='light grey',
 				command=lambda: press(6), height=1, width=7)
 button6.grid(row=3, column=2)
 
-button7 = Button(Frame_calc, text=' 7 ', fg='black', bg='red',
+button7 = Button(Frame_calc, text=' 7 ', fg='black', bg='light grey',
 				command=lambda: press(7), height=1, width=7)
-button7.grid(row=4, column=0)
+button7.grid(row=4, column=0,pady=2)
 
-button8 = Button(Frame_calc, text=' 8 ', fg='black', bg='red',
+button8 = Button(Frame_calc, text=' 8 ', fg='black', bg='light grey',
 				command=lambda: press(8), height=1, width=7)
 button8.grid(row=4, column=1)
 
-button9 = Button(Frame_calc, text=' 9 ', fg='black', bg='red',
+button9 = Button(Frame_calc, text=' 9 ', fg='black', bg='light grey',
 				command=lambda: press(9), height=1, width=7)
 button9.grid(row=4, column=2)
 
-button0 = Button(Frame_calc, text=' 0 ', fg='black', bg='red',
+button0 = Button(Frame_calc, text=' 0 ', fg='black', bg='light grey',
 				command=lambda: press(0), height=1, width=7)
-button0.grid(row=5, column=0)
+button0.grid(row=5, column=0, pady=2)
 
-plus = Button(Frame_calc, text=' + ', fg='black', bg='red',
+plus = Button(Frame_calc, text=' + ', fg='black', bg='light grey',
 				command=lambda: press("+"), height=1, width=7)
 plus.grid(row=2, column=3)
 
-minus = Button(Frame_calc, text=' - ', fg='black', bg='red',
+minus = Button(Frame_calc, text=' - ', fg='black', bg='light grey',
 				command=lambda: press("-"), height=1, width=7)
 minus.grid(row=3, column=3)
 
-multiply = Button(Frame_calc, text=' * ', fg='black', bg='red',
+multiply = Button(Frame_calc, text=' * ', fg='black', bg='light grey',
 				command=lambda: press("*"), height=1, width=7)
 multiply.grid(row=4, column=3)
 
-divide = Button(Frame_calc, text=' / ', fg='black', bg='red',
+divide = Button(Frame_calc, text=' / ', fg='black', bg='light grey',
 				command=lambda: press("/"), height=1, width=7)
 divide.grid(row=5, column=3)
 
-equal = Button(Frame_calc, text=' = ', fg='black', bg='red',
+equal = Button(Frame_calc, text=' = ', fg='black', bg='light grey',
 			command=equalpress, height=1, width=7)
 equal.grid(row=5, column=2)
 
-clear = Button(Frame_calc, text='Clear', fg='black', bg='red',
+clear = Button(Frame_calc, text='Clear', fg='black', bg='light grey',
 			command=clear, height=1, width=7)
 clear.grid(row=5, column='1')
 
-Decimal= Button(Frame_calc, text='.', fg='black', bg='red',
+Decimal= Button(Frame_calc, text='.', fg='black', bg='light grey',
 				command=lambda: press('.'), height=1, width=7)
 Decimal.grid(row=6, column=0)
 	# start the GUI
