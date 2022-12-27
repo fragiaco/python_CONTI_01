20deb14e6dd26b0322ed8a9ea055bbd7df418ef8
stefano <giacomo.internet@gmail.com>
2022-12-27 11:32:32 +0100

    conti camerino colonna Entrate_Uscite

--- conti_camerino.py
+++ conti_camerino.py
@@ -4,14 +4,13 @@ import pandas as pd
 #leggo e creo dataframe sensa indici colonna
 df_conti_camerino=pd.read_excel('conti_camerino_da_importare.xlsx', header=None)
 #verifico
-print(df_conti_camerino.info())
-print(df_conti_camerino.head())
+#print(df_conti_camerino.head())
 print() # riga vuota
 
 #imposto gli indici colonna
 df_conti_camerino.columns = ['Anno', 'Mese', 'Categoria', 'Voce','Euro']
 #verifico
-print(df_conti_camerino.head())
+#print(df_conti_camerino.head())
 print()
 
 #salvo il file con un nome diverso
@@ -20,10 +19,10 @@ df_conti_camerino.to_excel('conti_camerino_modified.xlsx')
 df_conti_camerino_modified = pd.read_excel('conti_camerino_modified.xlsx', index_col=0)
 
 # Stampo le prime 5 righe. Potrei anche usare .head()
-print(df_conti_camerino_modified.iloc[0:5])
+#print(df_conti_camerino_modified.iloc[0:5])
 
 #print(df_conti_camerino_modified.head())
-print(df_conti_camerino_modified.info())
+#print(df_conti_camerino_modified.info())
 
 #seleziona solo le colonne desiderate
 #wanted_columns = df_conti_camerino_modified[['Anno', 'Mese']]
@@ -33,9 +32,9 @@ print(df_conti_camerino_modified.info())
 
 
 #pulizia delle righe con valori nulli
-print(df_conti_camerino_modified.isnull().sum())
-print(df_conti_camerino_modified.dropna(inplace=True))
-print(df_conti_camerino_modified.isnull().sum())
+# print(df_conti_camerino_modified.isnull().sum())
+# print(df_conti_camerino_modified.dropna(inplace=True))
+#print(df_conti_camerino_modified.isnull().sum())
 
 
 
@@ -64,7 +63,38 @@ df_conti_camerino_modified['Entrate_Uscite']= df_conti_camerino_modified['Catego
                             else 'Uscite')
 
 
-df_conti_camerino_modified = df_conti_camerino_modified[['Anno', 'Mese','Entrate_Uscite', 'Categoria', 'Voce','Euro']]
-print(df_conti_camerino_modified.head())
+df_conti_camerino_modified = df_conti_camerino_modified [['Anno', 'Mese', 'Entrate_Uscite', 'Categoria', 'Voce','Euro']]
+#print(df_conti_camerino_modified.head())
 
-df_conti_camerino_modified.to_excel('conti_camerino_modified_excel.xlsx')
+#df_conti_camerino_modified.to_excel('conti_camerino_modified_excel.xlsx')
+
+########PIVOT
+import numpy as np
+#df_conti_camerino_modified['Anno'] = df_conti_camerino_modified['Anno'].astype(str)
+df_conti_camerino_modified["Mese"] = df_conti_camerino_modified["Mese"].astype("category")
+df_conti_camerino_modified["Mese"] = df_conti_camerino_modified["Mese"].cat.set_categories(["gennaio","febbraio","marzo","aprile","maggio","giugno","luglio","agosto","settembre","ottobre","novembre","dicembre"])
+
+# Example 1 - Using loc[] with multiple conditions
+                                                                    # df2=df.loc[(df['Discount'] >= 1000) & (df['Discount'] <= 2000)]
+#
+# # Example 2
+# df2=df.loc[(df['Discount'] >= 1200) | (df['Fee'] >= 23000 )]
+# print(df2)
+
+df_conti_camerino_pivot_tabellone_anno_entrate = df_conti_camerino_modified.loc[
+                                        (df_conti_camerino_modified['Anno'] == 2015) &
+                                        (df_conti_camerino_modified['Mese'] == 'gennaio') &
+                                        (df_conti_camerino_modified['Entrate_Uscite'] == 'Entrate')]
+
+print(df_conti_camerino_pivot_tabellone_anno_entrate.head())
+
+pivot_anno_entrate = np.round(pd.pivot_table
+                              (df_conti_camerino_pivot_tabellone_anno_entrate,
+                               values='Euro',
+                               index=['Entrate_Uscite','Categoria','Voce'],
+                               columns='Mese',
+                               fill_value=0,
+                               margins=True),2)
+
+print(pivot_anno_entrate)
+pivot_anno_entrate.to_excel('conti_camerino_modified_excel.xlsx')
