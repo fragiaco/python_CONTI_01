3e1ed2662dc5fcdf9b603f1e3719509298b49dcd
stefano <giacomo.internet@gmail.com>
2022-12-30 12:27:41 +0100

    formattazione openpyxl win

--- conti_camerino_01.py
+++ conti_camerino_01.py
@@ -1,48 +1,10 @@
-# provo ad importare conti_camerino
+
 import pandas as pd
-#leggo e creo dataframe sensa indici colonna
-df_conti_camerino=pd.read_excel('conti_camerino_da_importare.xlsx', header=None, index_col=None)
-#verifico
-#print(df_conti_camerino.head())
-print() # riga vuota
+import numpy as np
+from openpyxl.styles import Font, Alignment
+from openpyxl import load_workbook
 
-#imposto gli indici colonna
-df_conti_camerino.columns = ['Anno', 'Mese', 'Categoria', 'Voce','Euro']
-#verifico
-#print(df_conti_camerino.head())
-print()
-
-#salvo il file con un nome diverso
-df_conti_camerino.to_excel('conti_camerino_modified.xlsx')
-
-##############################################################
-# Leggo il nuovo file, creo nuovo dataframe senza colonna indice
 df_conti_camerino_modified = pd.read_excel('conti_camerino_modified.xlsx', index_col=0)
-
-# Stampo le prime 5 righe. Potrei anche usare .head()
-#print(df_conti_camerino_modified.iloc[0:5])
-
-#print(df_conti_camerino_modified.head())
-#print(df_conti_camerino_modified.info())
-
-#seleziona solo le colonne desiderate
-#wanted_columns = df_conti_camerino_modified[['Anno', 'Mese']]
-#salva il fle come hai fatto prima con un nome diverso .to_excel
-# Leggo il nuovo file, creo nuovo dataframe senza colonna indice
-
-
-
-#pulizia delle righe con valori nulli
-# print(df_conti_camerino_modified.isnull().sum())
-# print(df_conti_camerino_modified.dropna(inplace=True))
-#print(df_conti_camerino_modified.isnull().sum())
-
-
-
-# print(df_conti_camerino_modified.loc
-#       [df_conti_camerino_modified['Categoria']=='Sussidi'])
-
-
 
 df_conti_camerino_modified['Entrate_Uscite']= df_conti_camerino_modified['Categoria'].apply\
     (lambda x: 'Entrate'    if  x=='Vendite varie' or
@@ -65,26 +27,9 @@ df_conti_camerino_modified['Entrate_Uscite']= df_conti_camerino_modified['Catego
 
 
 df_conti_camerino_modified = df_conti_camerino_modified [['Anno', 'Mese', 'Entrate_Uscite', 'Categoria', 'Voce','Euro']]
-#print(df_conti_camerino_modified.head())
 
-#df_conti_camerino_modified.to_excel('conti_camerino_modified_excel.xlsx')
-
-########PIVOT
-import numpy as np
-#df_conti_camerino_modified['Anno'] = df_conti_camerino_modified['Anno'].astype(str)
 df_conti_camerino_modified["Mese"] = df_conti_camerino_modified["Mese"].astype("category")
 df_conti_camerino_modified["Mese"] = df_conti_camerino_modified["Mese"].cat.set_categories(["gennaio","febbraio","marzo","aprile","maggio","giugno","luglio","agosto","settembre","ottobre","novembre","dicembre"])
-
-# Example 1 - Using loc[] with multiple conditions
-                                                                    # df2=df.loc[(df['Discount'] >= 1000) & (df['Discount'] <= 2000)]
-#
-# # Example 2
-# df2=df.loc[(df['Discount'] >= 1200) | (df['Fee'] >= 23000 )]
-# print(df2)
-
-
-
-
 
 
 
@@ -100,9 +45,6 @@ df_conti_camerino_pivot_tabellone_anno_uscite = df_conti_camerino_modified.loc[
 
 
 
-
-print(df_conti_camerino_pivot_tabellone_anno_entrate.head())
-
 pivot_gennaio_entrate = np.round(pd.pivot_table
                             (df_conti_camerino_pivot_tabellone_anno_entrate,
                                values='Euro',
@@ -111,8 +53,6 @@ pivot_gennaio_entrate = np.round(pd.pivot_table
                                margins=True,
                                margins_name= 'TOTALE',
                                fill_value=0),2)
-
-
 
 
 pivot_gennaio_uscite = np.round(pd.pivot_table
@@ -124,21 +64,11 @@ pivot_gennaio_uscite = np.round(pd.pivot_table
                                margins_name= 'TOTALE',
                                fill_value=0),2)
 
-print(pivot_gennaio_entrate)
-print(pivot_gennaio_uscite)
-
-
-# Create a Pandas Excel writer using XlsxWriter as the engine.
-#writer = pd.ExcelWriter('conti_camerino_multiple.xlsx')
-
-# oppure
 
 with pd.ExcelWriter("conti_camerino_multiple.xlsx") as writer:
     pivot_gennaio_uscite.to_excel(writer, sheet_name='gennaio_uscite')
     pivot_gennaio_entrate.to_excel(writer, sheet_name='gennaio_entrate')
 
-# with pd.ExcelWriter("conti_camerino_multiple.xlsx", mode="a", engine="openpyxl") as writer:
-#     df_conti_camerino_pivot_tabellone_anno_entrate.to_excel(writer, sheet_name="mode a")  # doctest: +SKIP
 
 with pd.ExcelWriter("conti_camerino_multiple.xlsx",
                     mode="a",
@@ -148,12 +78,7 @@ with pd.ExcelWriter("conti_camerino_multiple.xlsx",
                     pivot_gennaio_entrate.to_excel(writer, sheet_name="multiple", startrow=5)
                     pivot_gennaio_uscite.to_excel(writer, sheet_name="multiple", startrow=(len(pivot_gennaio_entrate)+10))
 
-########################
-########################
-from openpyxl.styles import colors
-from openpyxl.styles import Font, Color
-from openpyxl.styles import Border, Side, PatternFill, Font, GradientFill, Alignment
-from openpyxl import load_workbook
+
 wb = load_workbook(filename = "conti_camerino_multiple.xlsx")
 ws_entrate = wb['gennaio_entrate']
 ws_uscite=wb['gennaio_uscite']
@@ -168,22 +93,8 @@ ws_multiple.column_dimensions['B'].width = 20
 ws_multiple.column_dimensions['C'].width = 30
 ws_multiple.column_dimensions['D'].width = 15
 
-
-from openpyxl.styles import numbers
-
-#ws_multiple['D21'].number_format = '_-* #.##0,00 €_-;-* #.##0,00 €_-;_-* "-"?? €_-;_-@_-'
-# ws_multiple['D21'].number_format = '_-* #.##0,00 €_-;-* #.##0,00 €_-;_-* "-"?? €_-;_-@_-'
-# ws_multiple.number_format = numbers.FORMAT_PERCENTAGE
 center_align = Alignment(horizontal='center', vertical='center')
-###################
 
-
-
-
-# for c in ws_multiple['D']:
-#     # c[0].border = no_right_side
-#     # c[0].alignment = center_align
-#     ws_multiple.number_format = '_-* #.##0,00 €_-;-* #.##0,00 €_-;_-* "-"?? €_-;_-@_-'
 
 ws_multiple.merge_cells('A1:D1')
 top_left_cell = ws_multiple['A1']
