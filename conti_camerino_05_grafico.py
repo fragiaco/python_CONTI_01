54f5b41404567ea7d40a1dd1d2b694c916553241
stefano <giacomo.internet@gmail.com>
2023-01-06 21:29:14 +0100

    conti_camerino_05 chart

--- conti_camerino_05_grafico.py
+++ conti_camerino_05_grafico.py
@@ -265,6 +265,145 @@ list_pivot_mese_uscite= [list_pivot_mese_uscite[0],
                     list_pivot_mese_uscite[11]
                     ]
 
+######################  grafico
+from datetime import date
+
+from openpyxl import Workbook
+from openpyxl.chart import (
+    LineChart,
+    Reference,
+)
+from openpyxl.chart.axis import DateAxis
+
+wb = Workbook()
+ws = wb.active
+
+
+rows = [
+    ['Date', 'Batch 1', 'Batch 2', 'Batch 3'],
+    ['gennaio', list_df_conti_camerino_mese_entrate[0]['Euro'].sum(numeric_only=True),
+                list_df_conti_camerino_mese_uscite[0]['Euro'].sum(numeric_only=True),
+                ((list_df_conti_camerino_mese_entrate[0]['Euro']).sum(numeric_only=True) -
+                (list_df_conti_camerino_mese_uscite[0]['Euro']).sum(numeric_only=True))
+     ],
+    ['febbraio',list_df_conti_camerino_mese_entrate[1]['Euro'].sum(numeric_only=True),
+                list_df_conti_camerino_mese_uscite[1]['Euro'].sum(numeric_only=True),
+                ((list_df_conti_camerino_mese_entrate[1]['Euro']).sum(numeric_only=True) -
+                (list_df_conti_camerino_mese_uscite[1]['Euro']).sum(numeric_only=True))
+     ],
+    ['marzo',   list_df_conti_camerino_mese_entrate[2]['Euro'].sum(numeric_only=True),
+                list_df_conti_camerino_mese_uscite[2]['Euro'].sum(numeric_only=True),
+                ((list_df_conti_camerino_mese_entrate[2]['Euro']).sum(numeric_only=True) -
+                (list_df_conti_camerino_mese_uscite[2]['Euro']).sum(numeric_only=True))
+     ],
+    ['aprile',  list_df_conti_camerino_mese_entrate[3]['Euro'].sum(numeric_only=True),
+                list_df_conti_camerino_mese_uscite[3]['Euro'].sum(numeric_only=True),
+                ((list_df_conti_camerino_mese_entrate[3]['Euro']).sum(numeric_only=True) -
+                (list_df_conti_camerino_mese_uscite[3]['Euro']).sum(numeric_only=True))
+     ],
+    ['maggio',  list_df_conti_camerino_mese_entrate[4]['Euro'].sum(numeric_only=True),
+                list_df_conti_camerino_mese_uscite[4]['Euro'].sum(numeric_only=True),
+                ((list_df_conti_camerino_mese_entrate[4]['Euro']).sum(numeric_only=True) -
+                (list_df_conti_camerino_mese_uscite[4]['Euro']).sum(numeric_only=True))
+     ],
+    ['giugno',  list_df_conti_camerino_mese_entrate[5]['Euro'].sum(numeric_only=True),
+                list_df_conti_camerino_mese_uscite[5]['Euro'].sum(numeric_only=True),
+                ((list_df_conti_camerino_mese_entrate[5]['Euro']).sum(numeric_only=True) -
+                (list_df_conti_camerino_mese_uscite[5]['Euro']).sum(numeric_only=True))
+     ],
+    ['luglio',  list_df_conti_camerino_mese_entrate[6]['Euro'].sum(numeric_only=True),
+                list_df_conti_camerino_mese_uscite[6]['Euro'].sum(numeric_only=True),
+                ((list_df_conti_camerino_mese_entrate[6]['Euro']).sum(numeric_only=True) -
+                (list_df_conti_camerino_mese_uscite[6]['Euro']).sum(numeric_only=True))
+     ],
+    ['agosto',  list_df_conti_camerino_mese_entrate[7]['Euro'].sum(numeric_only=True),
+                list_df_conti_camerino_mese_uscite[7]['Euro'].sum(numeric_only=True),
+                ((list_df_conti_camerino_mese_entrate[7]['Euro']).sum(numeric_only=True) -
+                (list_df_conti_camerino_mese_uscite[7]['Euro']).sum(numeric_only=True))
+     ],
+    ['settembre',list_df_conti_camerino_mese_entrate[8]['Euro'].sum(numeric_only=True),
+                list_df_conti_camerino_mese_uscite[8]['Euro'].sum(numeric_only=True),
+                ((list_df_conti_camerino_mese_entrate[8]['Euro']).sum(numeric_only=True) -
+                (list_df_conti_camerino_mese_uscite[8]['Euro']).sum(numeric_only=True))
+     ],
+    ['ottobre', list_df_conti_camerino_mese_entrate[9]['Euro'].sum(numeric_only=True),
+                list_df_conti_camerino_mese_uscite[9]['Euro'].sum(numeric_only=True),
+                ((list_df_conti_camerino_mese_entrate[9]['Euro']).sum(numeric_only=True) -
+                (list_df_conti_camerino_mese_uscite[9]['Euro']).sum(numeric_only=True))
+     ],
+    ['novembre',list_df_conti_camerino_mese_entrate[10]['Euro'].sum(numeric_only=True),
+                list_df_conti_camerino_mese_uscite[10]['Euro'].sum(numeric_only=True),
+                ((list_df_conti_camerino_mese_entrate[10]['Euro']).sum(numeric_only=True) -
+                (list_df_conti_camerino_mese_uscite[10]['Euro']).sum(numeric_only=True))
+     ],
+    ['dicembre',list_df_conti_camerino_mese_entrate[11]['Euro'].sum(numeric_only=True),
+                list_df_conti_camerino_mese_uscite[11]['Euro'].sum(numeric_only=True),
+                ((list_df_conti_camerino_mese_entrate[11]['Euro']).sum(numeric_only=True) -
+                (list_df_conti_camerino_mese_uscite[11]['Euro']).sum(numeric_only=True))
+     ],
+        ]
+
+for row in rows:
+    ws.append(row)
+
+c1 = LineChart()
+c1.title = "Line Chart"
+c1.style = 13
+c1.y_axis.title = 'Size'
+c1.x_axis.title = 'Test Number'
+
+data = Reference(ws, min_col=2, min_row=1, max_col=4, max_row=13)
+c1.add_data(data, titles_from_data=True)
+
+# Style the lines
+s1 = c1.series[0]
+s1.marker.symbol = "triangle"
+s1.marker.graphicalProperties.solidFill = "FF0000" # Marker filling
+s1.marker.graphicalProperties.line.solidFill = "FF0000" # Marker outline
+
+s1.graphicalProperties.line.noFill = True
+
+s2 = c1.series[1]
+s2.graphicalProperties.line.solidFill = "00AAAA"
+s2.graphicalProperties.line.dashStyle = "sysDot"
+s2.graphicalProperties.line.width = 100050 # width in EMUs
+
+s2 = c1.series[2]
+s2.smooth = True # Make the line smooth
+
+ws.add_chart(c1, "A10")
+
+from copy import deepcopy
+stacked = deepcopy(c1)
+stacked.grouping = "stacked"
+stacked.title = "Stacked Line Chart"
+ws.add_chart(stacked, "A27")
+
+percent_stacked = deepcopy(c1)
+percent_stacked.grouping = "percentStacked"
+percent_stacked.title = "Percent Stacked Line Chart"
+ws.add_chart(percent_stacked, "A44")
+
+# Chart with date axis
+c2 = LineChart()
+c2.title = "Date Axis"
+c2.style = 12
+c2.y_axis.title = "Size"
+c2.y_axis.crossAx = 500
+c2.x_axis = DateAxis(crossAx=100)
+c2.x_axis.number_format = 'd-mmm'
+c2.x_axis.majorTimeUnit = "days"
+c2.x_axis.title = "Date"
+
+c2.add_data(data, titles_from_data=True)
+dates = Reference(ws, min_col=1, min_row=2, max_row=13)
+c2.set_categories(dates)
+
+ws.add_chart(c2, "A61")
+
+wb.save("line.xlsx")
+
+
 ##############PRIMA PAGINA
 import openpyxl
 from openpyxl.drawing.image import Image
